6c739338b25206e13f1613658364b192de02ab44
Dario Spaziani <dario.spaziani94@gmail.com>
2024-02-18 18:38:45 +0100

    Added day night cycle

--- Jurassic-Survival-Frontier/Assets/Scripts/PlayerMovement.cs
+++ Jurassic-Survival-Frontier/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    [SerializeField] private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
