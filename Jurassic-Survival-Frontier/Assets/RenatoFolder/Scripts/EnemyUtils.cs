c4d97d6126e677c16a97c8e71f38531d0effee19
sciwiz12 <ricardopenamcknight@gmail.com>
2024-02-21 08:53:37 -0500

    Delete Jurassic-Survival-Frontier/Assets directory
    Deleting Asset Directory

--- Jurassic-Survival-Frontier/Assets/RenatoFolder/Scripts/EnemyUtils.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyUtils : MonoBehaviour
-{
-    public Transform playerPos;
-}
--- Jurassic-Survival-Frontier/Assets/RicksFolder/RicksScripts/GhostItem.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GhostItem : MonoBehaviour
-{
-    public InventorySlot originalSlot; // Reference to the slot from which the ghost was created
-}
-
